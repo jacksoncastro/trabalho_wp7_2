6db733690c4961752b67d70c33fac7f0af340555
Francisco Jackson da Silva Castro <jack.vasc@yahoo.com.br>
2017-09-01 23:22:11 -0300

    Add speech

--- Trabalho_wp7_2/ListUsers.xaml.cs
+++ Trabalho_wp7_2/ListUsers.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.SpeechSynthesis;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -28,6 +29,29 @@ namespace Trabalho_wp7_2
             this.InitializeComponent();
 
             this.Users = LoginManager.Users;
+        }
+
+        private void usersListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            User user = e.ClickedItem as User;
+            this.Speech(user.Name);
+        }
+
+
+        private async void Speech(String text)
+        {
+            // The media object for controlling and playing audio.
+            MediaElement mediaElement = new MediaElement();
+
+            // The object for controlling the speech synthesis engine (voice).
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+
+            // Generate the audio stream from plain text.
+            SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(text);
+
+            // Send the stream to the media object.
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
         }
     }
 }
