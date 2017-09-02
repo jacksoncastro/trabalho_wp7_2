e51b5ab0980bb9eb0c0d01a1bcc7c23c9eaf5607
Francisco Jackson da Silva Castro <jack.vasc@yahoo.com.br>
2017-09-02 10:01:26 -0300

    Add video

---
+++ Trabalho_wp7_2/VideoLearn.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+namespace Trabalho_wp7_2
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class VideoLearn : Page
+    {
+        public VideoLearn()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            media.Play();
+        }
+
+        private void Button_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            media.Stop();
+        }
+    }
+}
